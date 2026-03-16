25ebc3e10b3f94a8f915989d8d056b7f7698b3bd
Alina <alinazolotopup99@gmail.com>
2026-03-16 19:05:17 +0100

    added code

---
+++ src/server.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import cors from "cors";
+import helmet from "helmet";
+import pino from 'pino-http';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.use(helmet());
+app.use(express.json());
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: "Retrieved all notes" });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const noteId = req.params.noteId;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+app.get("/test-error", (req, res) => {
+  throw new Error("Something went wrong");
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Rout not found' });
+});
+
+app.use((err, req, res, next) => {
+  const isProd = process.env.NODE_ENV === 'production';
+  res.status(500).json({
+    message: isProd ? "Something went wrong. Please try again later." : err.stack,
+});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
