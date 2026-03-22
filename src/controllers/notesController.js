f189351c8b285ec776bf47f2019f4e933e246b69
Alina <alinazolotopup99@gmail.com>
2026-03-22 14:52:59 +0100

    changed router

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,7 +1,7 @@
 import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
 
-export const getNotes = async (req, res) => {
+export const getAllNotes = async (req, res) => {
   const notes = await Note.find();
   res.status(200).json(notes);
 };
@@ -11,14 +11,14 @@ export const getNoteById = async (req, res) => {
   const note = await Note.findById(noteId);
 
   if (!note) {
-    throw createHttpError('Student not found');
+    throw createHttpError(404, 'Note not found');
   }
   res.status(200).json(note);
 };
 
 export const createNote = async (req, res) => {
   const note = await Note.create(req.body);
-  res.status(200).json(note);
+  res.status(201).json(note);
 };
 
 export const deleteNote = async (req, res) => {
@@ -44,7 +44,7 @@ export const updateNote = async (req, res) => {
   );
 
   if (!note) {
-	throw createHttpError(404, 'Student not found');
+	throw createHttpError(404, 'Note not found');
   }
 
   res.status(200).json(note);
